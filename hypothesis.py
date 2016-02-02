7d683ba9cdbdc56348d27447d3d42be3e0581d8d
Tom Gillespie <tgbugs@gmail.com>
2016-02-01 16:10:45 -0800

    added HypothesisAnnotation class from Jon's repo including python3 compat changes

--- hypothesis.py
+++ hypothesis.py
@@ -21,6 +21,8 @@ class HypothesisUtils:
         self.username = username
         self.password = password
         self.group = group if group is not None else '__world__'
+        self.single_page_limit = 200 if limit is None else limit  # per-page, the api honors limit= up to (currently) 200
+        self.multi_page_limit = 200 if max_results is None else max_results  # limit for paginated results
         self.permissions = {
                 "read": ['group:' + self.group],
                 "update": ['acct:' + self.username + '@hypothes.is'],
@@ -97,3 +99,113 @@ class HypothesisUtils:
         r = requests.post(self.api_url + '/annotations', headers=headers, data=data.encode('utf-8'))
         return r
 
+    def search_all(self, params={}):
+        """Call search API with pagination, return rows """
+        params['offset'] = 0
+        params['limit'] = self.single_page_limit
+        while True:
+            h_url = self.query_url.format(query=urlencode(params, True))
+            obj = self.authenticated_api_query(h_url)
+            rows = obj['rows']
+            row_count = len(rows)
+            if 'replies' in obj:
+               rows += obj['replies']
+            params['offset'] += row_count
+            if params['offset'] > self.multi_page_limit:
+                break
+            if len(rows) is 0:
+                break
+            for row in rows:
+                yield row
+
+class HypothesisAnnotation:
+    """Encapsulate one row of a Hypothesis API search."""
+    def __init__(self, row):
+        self.type = None
+        self.id = row['id']
+        self.updated = row['updated'][0:19]
+        self.user = row['user'].replace('acct:','').replace('@hypothes.is','')
+
+        if 'uri' in row:    # should it ever not?
+            self.uri = row['uri']
+        else:
+             self.uri = "no uri field for %s" % self.id
+        self.uri = self.uri.replace('https://via.hypothes.is/h/','').replace('https://via.hypothes.is/','')
+
+        if self.uri.startswith('urn:x-pdf') and 'document' in row:
+            if 'link' in row['document']:
+                self.links = row['document']['link']
+                for link in self.links:
+                    self.uri = link['href']
+                    if self.uri.encode('utf-8').startswith('urn:') == False:
+                        break
+            if self.uri.encode('utf-8').startswith('urn:') and 'filename' in row['document']:
+                self.uri = row['document']['filename']
+
+        if 'document' in row and 'title' in row['document']:
+            t = row['document']['title']
+            if isinstance(t, list) and len(t):
+                self.doc_title = t[0]
+            else:
+                self.doc_title = t
+        else:
+            self.doc_title = self.uri
+        if self.doc_title is None:
+            self.doc_title = ''
+        self.doc_title = self.doc_title.replace('"',"'")
+        if self.doc_title == '': self.doc_title = 'untitled'
+
+        self.tags = []
+        if 'tags' in row and row['tags'] is not None:
+            self.tags = row['tags']
+            if isinstance(self.tags, list):
+                self.tags = [t.strip() for t in self.tags]
+
+        self.text = ''
+        if 'text' in row:
+            self.text = row['text']
+
+        self.references = []
+        if 'references' in row:
+            self.type = 'reply'
+            self.references = row['references']
+
+        self.target = []
+        if 'target' in row:
+            self.target = row['target']
+
+        self.is_page_note = False
+        try:
+            if self.references == [] and self.target is not None and len(self.target) and isinstance(self.target,list) and 'selector' not in self.target[0]:
+                self.is_page_note = True
+                self.type = 'pagenote'
+        except:
+            traceback.print_exc()
+        if 'document' in row and 'link' in row['document']:
+            self.links = row['document']['link']
+            if not isinstance(self.links, list):
+                self.links = [{'href':self.links}]
+        else:
+            self.links = []
+
+        self.start = self.end = self.prefix = self.exact = self.suffix = None
+        try:
+            if isinstance(self.target,list) and len(self.target) and 'selector' in self.target[0]:
+                self.type = 'annotation'
+                selectors = self.target[0]['selector']
+                for selector in selectors:
+                    if 'type' in selector and selector['type'] == 'TextQuoteSelector':
+                        try:
+                            self.prefix = selector['prefix']
+                            self.exact = selector['exact']
+                            self.suffix = selector['suffix']
+                        except:
+                            traceback.print_exc()
+                    if 'type' in selector and selector['type'] == 'TextPositionSelector' and 'start' in selector:
+                        self.start = selector['start']
+                        self.end = selector['end']
+                    if 'type' in selector and selector['type'] == 'FragmentSelector' and 'value' in selector:
+                        self.fragment_selector = selector['value']
+
+        except:
+            print(traceback.format_exc())
