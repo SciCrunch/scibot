58bad297473a6f63a77c30a7d294e0fa523963b0
Tom Gillespie <tgbugs@gmail.com>
2018-09-05 17:32:28 -0700

    import renaming for guni-dash

--- opt/guni-dash.sh
+++ opt/guni-dash.sh
@@ -1,1 +1,1 @@
-gunicorn -b localhost:5005 -n scibot-dashboard -w 2 -k gevent -t 600 --preload --log-level debug dash:app
+gunicorn -b localhost:5005 -n scibot-dashboard -w 2 -k gevent -t 600 --preload --log-level debug scibot.dash:app
--- scibot/dash.py
+++ scibot/dash.py
@@ -1,5 +1,5 @@
 from gevent import monkey
 monkey.patch_all()
 
-from dashboard import setup
+from scibot.dashboard import setup
 app = setup()
