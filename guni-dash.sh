3a201f6799a081fcbcf3a12e55ff63900ae9e97e
Tom Gillespie <tgbugs@gmail.com>
2018-09-05 17:20:50 -0700

    guni-dash decreased the number of workers to reduce memory load

--- guni-dash.sh
+++ guni-dash.sh
@@ -1,1 +1,1 @@
-gunicorn -b localhost:5005 -n scibot-dashboard -w 4 -k gevent -t 600 --preload --log-level debug dash:app
+gunicorn -b localhost:5005 -n scibot-dashboard -w 2 -k gevent -t 600 --preload --log-level debug dash:app
